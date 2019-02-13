be714ba2bfdad503769d87b87269514ce377aaaa
Udesh Kumarasinghe <mail@udeshuk.me>
2019-02-13 09:14:40 +0530

    Fed api information to base routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send({});
+  res.send({
+    title: 'labellab',
+    routes: [
+      {route: "/classify", description: "Classification API"}
+    ]
+  });
 });
 
 module.exports = router;
