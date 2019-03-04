041c385e341e1058d41b89566ef6a5fb8b0f234a
Udesh Kumarasinghe <mail@udeshuk.me>
2019-03-04 20:29:29 +0530

    Implemented auth and refactored

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,38 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var fileUpload = require('express-fileupload');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const fileUpload = require('express-fileupload');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-var config = require('../app.config')
-var indexRouter = require('./routes/index');
-var classifyRouter = require('./routes/classify');
+const config = require('../app.config')
+const indexRouter = require('./api/index');
+const classifyRouter = require('./api/classify');
+const authRouter = require('./auth/auth');
 
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(fileUpload());
 
-app.use('/', indexRouter);
-app.use('/classify', classifyRouter);
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.DB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  }).catch(err => {
+    console.log('Could not connect to MongoDB.');
+    console.log(err);
+    process.exit();
+  });
+
+app.use(config.VERSION, indexRouter);
+app.use(config.VERSION + '/auth', authRouter);
+app.use(config.VERSION + '/classify', classifyRouter);
 
 app.set('port', process.env.PORT || 3000);
 module.exports = app;
